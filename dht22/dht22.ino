04d01b0c4eea0be75aa06ad74d9b2551d7d961fc
lwobben <liana_d_w@hotmail.com>
2023-06-04 16:16:15 +0200

    main version of dht22

--- dht22/dht22.ino
+++ dht22/dht22.ino
@@ -1,48 +1,48 @@
+// libraries
 #include "DHT.h"
 #include "ESP8266WiFi.h"
 #include "aREST.h"
 #include "secrets.h"
 
+// vars
+const char* ssid = SECRET_SSID;
+const char* password = SECRET_PASS;
 #define DHTPIN D2
 #define DHTTYPE DHT22
 #define LISTEN_PORT 80
-
-const char* ssid = SECRET_SSID;
-const char* password = SECRET_PASS;
 float humidity;
 float temperature;
 
+// set instances
 WiFiServer server(LISTEN_PORT);
 aREST rest = aREST();
 DHT dht(DHTPIN, DHTTYPE);
 
-void startServer() {
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-  }
-  Serial.println("WiFi connected");
-  
-  server.begin();
-  Serial.println("Server started");
-  Serial.println(WiFi.localIP());
-}
-
+// run once
 void setup() {
   Serial.begin(9600);
   Serial.println("Start setup");
-  pinMode(LED_BUILTIN, OUTPUT);
   dht.begin();
 
+  // REST API vars
   rest.variable("humidity",&humidity);
   rest.variable("temperature",&temperature);
   rest.set_id("1");
-  rest.set_name("esp8266_liana");
+  rest.set_name("esp8266");
   
-  startServer();
+  // connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(200);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  
+  // start the server
+  server.begin();
+  Serial.println("Server started");
+  Serial.println(WiFi.localIP());
 }
 
 // repeat
@@ -50,12 +50,9 @@ void loop() {
   humidity = dht.readHumidity();
   temperature = dht.readTemperature();
   
-  while (WiFi.status() != WL_CONNECTED) {
-    startServer();
-  }
-  
+  // handle REST calls
   WiFiClient client = server.available();
-  while (!client) {
+  if (!client) {
     return;
   }
   while(!client.available()){
